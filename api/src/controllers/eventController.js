642d3f53e76da203e7381200824dd7cab1e290ab
Đào Anh Tuấn <106532352+anhtuandao1311@users.noreply.github.com>
2023-07-25 18:53:17 +0700

    Update eventController.js

--- api/src/controllers/eventController.js
+++ api/src/controllers/eventController.js
@@ -4,7 +4,8 @@ const createEvent = async (req,res)=>{
   try{
     console.log(req.body)
     await eventService.createEvent()
-
+    const res = await eventService.createEvent()
+    return res.status(200).json(res)
   } catch(e){
     return res.status(400).json({
       message:e
